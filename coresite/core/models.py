d0447ae9d65d126a768508fe57ad65aaa89a14ac
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-09 23:17:29 +0000

    modified core/models.py

--- coresite/core/models.py
+++ coresite/core/models.py
@@ -6,3 +6,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Core(models.Model):
     title = models.CharField(max_length=200)
+    excerpt = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='core')
+    slug = models.SlugField(max_length=100, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(default=timezone.now)
+
+    def get_absolute_urls(self):
+        return reverse('core:single', args=[self.slug])
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published']
