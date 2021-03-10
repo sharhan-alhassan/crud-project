c7b648cc18e25ad96e61bbcc82e06b1d0b3dfeb6
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-10 23:30:56 +0000

    edited

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-## pip install django-widgets-tweaks
+
 *Notes*
 1. This repo is the implemention of a basic CRUD app
 2. It is based on [Very Academy's](https://www.youtube.com/watch?v=RwWhQTSV44Q) tutorial 
 3. The `navbar` and the `footer` are different from what he used in the tutorial. You can decide to use yours too
 4. This repo performs all the core functionalities as shown in the tutorial 
+5. I used a random photo api to generate the photos 🙂
+
+### pip install django-widgets-tweaks
+
--- coresite/coresite/settings.py
+++ coresite/coresite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import  os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@2gbqe9c(dtg%2vm)q-#q(sa_qq82&-l7pq4kg9w!0wz)^!2uv'
+SECRET_KEY = os.environ.get('CRUD_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
